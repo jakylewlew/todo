43ba3550951baba458fcc260d9dcf302809e62e9
jakylewlew <jake.lewis22@outlook.com>
2019-02-08 12:39:17 -0600

    "Here we Go

--- todolist.py
+++ todolist.py
@@ -13,7 +13,7 @@ read_list.pop(len(read_list)-1)
 todolist = []
 i = 0
 while(i != len(read_list)):
-    module_task = []q
+    module_task = []
     module_task.append(read_list[i])
     module_task.append(read_list[i+1])
     module_task.append(read_list[i+2])
@@ -88,7 +88,7 @@ def write_file():
     todo_file.close()
 def print_pretty():
     for item in todolist:
-        print('{0:30s} : {1:10s}'.format(item[0], item[1]))
+        print('{0:30s} : {1:10s} : {2:3}'.format(item[0], item[1], item[2]))
 inp = 0
 while inp != 'q': 
     os.system("clear") 
